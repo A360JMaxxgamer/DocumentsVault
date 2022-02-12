b9e1eeecc8642b04196ce7cdaf3888f1932ea6b1
Marco Hamann <marcohamann89@hotmail.de>
2022-02-12 17:38:16 +0100

    add MockExtension.cs to simplify unit test mock setup

---
+++ src/Shared/UnitTestHelper/Extensions/MockExtension.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+using Moq;
+
+namespace UnitTestHelper.Extensions;
+
+public static class MockExtension
+{
+    /// <summary>
+    /// Setups <paramref name="expression"/> on <paramref name="mock"/> to return the mocked
+    /// object instance of the returned mock.
+    /// </summary>
+    /// <param name="mock"></param>
+    /// <param name="expression"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="TResult"></typeparam>
+    /// <returns>The mock which holds the object which is returned by the expression.</returns>
+    public static Mock<TResult> SetupReturnMock<T, TResult>(this Mock<T> mock, Expression<Func<T, TResult>> expression)
+        where T : class
+        where TResult : class
+    {
+        var resultMock = new Mock<TResult>();
+        mock
+            .Setup(expression)
+            .Returns(resultMock.Object);
+        return resultMock;
+    }
+}
