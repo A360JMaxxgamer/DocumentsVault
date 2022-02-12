974b9bad637df60b77382b4a958c5e1d5d9a46d6
Marco Hamann <marcohamann89@hotmail.de>
2022-02-12 17:38:38 +0100

    add MongoIndexerTest.cs

---
+++ src/Files/Files.Service.Tests/Handlers/MongoIndexerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Files.Service.Handlers;
+using Files.Service.Models;
+using MongoDB.Driver;
+using Moq;
+using UnitTestHelper.Extensions;
+using Xunit;
+
+namespace Files.Service.Tests.Handlers;
+
+public class MongoIndexerTest
+{
+    [Fact]
+    public async Task Should_InsertAsync_Returns_Result()
+    {
+        var mongoClientMock = new Mock<IMongoClient>();
+        mongoClientMock
+            .SetupReturnMock(client => client.GetDatabase(It.IsAny<string>(), It.IsAny<MongoDatabaseSettings>()))
+            .SetupReturnMock(db =>
+                db.GetCollection<UploadFile>(It.IsAny<string>(), It.IsAny<MongoCollectionSettings>()));
+            
+        var mongoIndexer = new MongoIndexer(mongoClientMock.Object);
+        const string originalFileName = "original";
+        const string fileName = "orig";
+
+        // Act
+        var result = await mongoIndexer.InsertAsync(Guid.NewGuid(), fileName, originalFileName);
+
+        // Assert
+        Assert.Equal(fileName, result.FileName);
+        Assert.Equal(originalFileName, result.OriginalFileName);
+    }
+}
