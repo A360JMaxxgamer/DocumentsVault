191b964b5e3f86646d3500565e5e21ca3b31d03c
Marco Hamann <marcohamann89@hotmail.de>
2022-02-13 22:42:39 +0100

    add models and GraphQL classes

--- Documents.Service/GraphQL/Mutation.cs
+++ Documents.Service/GraphQL/Mutation.cs
@@ -1,6 +1,21 @@
-﻿namespace Documents.Service.GraphQL;
+﻿using Documents.Service.Models;
+
+namespace Documents.Service.GraphQL;
 
 public class Mutation
 {
+    /// <summary>
+    /// Create a new document
+    /// </summary>
+    /// <param name="fileIds">A list of all associated files</param>
+    /// <param name="metadata">Metadata of document</param>
+    /// <returns>Created document</returns>
+    public Document AddDocument(List<Guid> fileIds, Metadata metadata) => throw new NotImplementedException();
     
+    /// <summary>
+    /// Deletes a document by its id
+    /// </summary>
+    /// <param name="documentId">Id of the document to delete</param>
+    /// <exception cref="NotImplementedException"></exception>
+    public void DeleteDocument(Guid documentId) => throw new NotImplementedException();
 }
---
+++ Documents.Service/Models/Document.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Documents.Service.Models;
+
+/// <summary>
+/// Document which contains various information e.g. a letter or
+/// </summary>
+public record Document
+{
+    /// <summary>
+    /// Id of the document
+    /// </summary>
+    [BsonId]
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Date the document was initially created
+    /// </summary>
+    public DateTime CreationDate { get; set; }
+    
+    /// <summary>
+    /// Date the document was modified last 
+    /// </summary>
+    public DateTime ModificationDate { get; set; }
+
+    /// <summary>
+    /// Additional metadata of the document
+    /// </summary>
+    public Metadata Metadata { get; set; } = new();
+
+    /// <summary>
+    /// Ids of the files
+    /// </summary>
+    public List<Guid> FileIds { get; set; } = new();
+}
