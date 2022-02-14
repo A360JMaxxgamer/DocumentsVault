1947283f5087f31468a2c4069692fdd6268f22c8
Marco Hamann <marcohamann89@hotmail.de>
2022-02-14 10:43:40 +0100

    add empty methods to Mutation.cs

--- Documents.Service/GraphQL/Mutation.cs
+++ Documents.Service/GraphQL/Mutation.cs
@@ -17,5 +17,38 @@ public class Mutation
     /// </summary>
     /// <param name="documentId">Id of the document to delete</param>
     /// <exception cref="NotImplementedException"></exception>
-    public void DeleteDocument(Guid documentId) => throw new NotImplementedException();
+    /// <returns>Updated document</returns>
+    public Document DeleteDocument(Guid documentId) => throw new NotImplementedException();
+
+    /// <summary>
+    /// Adds <paramref name="tags"/> to the document with the <paramref name="documentId"/>
+    /// </summary>
+    /// <param name="documentId">Id of the document</param>
+    /// <param name="tags">Tags to add</param>
+    /// <returns>Updated document</returns>
+    public Document AddTags(Guid documentId, string[] tags) => throw new NotImplementedException();
+    
+    /// <summary>
+    /// Deletes <paramref name="tags"/> off the document with the <paramref name="documentId"/>
+    /// </summary>
+    /// <param name="documentId">Id of the document</param>
+    /// <param name="tags">Tags to delete</param>
+    /// <returns>Updated document</returns>
+    public Document DeleteTags(Guid documentId, string[] tags) => throw new NotImplementedException();
+
+    /// <summary>
+    /// Updates the title of the document
+    /// </summary>
+    /// <param name="documentId">Id of the document</param>
+    /// <param name="title">New title</param>
+    /// <returns>Updated document</returns>
+    public Document UpdateDocumentTitle(Guid documentId, string title) => throw new NotImplementedException();
+
+    /// <summary>
+    /// Updates the text of the document
+    /// </summary>
+    /// <param name="documentId">Id of the document</param>
+    /// <param name="text">New text</param>
+    /// <returns>Updated document</returns>
+    public Document UpdateDocumentText(Guid documentId, string text) => throw new NotImplementedException();
 }
